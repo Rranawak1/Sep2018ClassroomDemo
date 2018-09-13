8605b404ed4e662c5a365b1c5f0e97db4e1d1eb3
Rranawak1 <43081663+Rranawak1@users.noreply.github.com>
2018-09-12 20:48:56 -0600

    Bunch of stuff
    Did a bunch of of stuff I can't remember
    Look through m8ty

---
+++ ChinookCS/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Artist> ALbum_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Artist Album_Find(int artistID)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistID);
+            }
+        }
+    }
+}
